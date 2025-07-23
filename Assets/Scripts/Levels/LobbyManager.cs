182bd295306410e27e693732ceb816cb19af010d
Seb18456 <109159484+Nightbreaker18@users.noreply.github.com>
2025-07-23 17:06:23 -0400

    Moved all Code into LobbyManager from RenderHelper
    - RenderHelper will be for much more general methods going forward

--- Assets/Scripts/Levels/LobbyManager.cs
+++ Assets/Scripts/Levels/LobbyManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using static Assets.Scripts.RenderHelper;
@@ -9,20 +10,68 @@ namespace Assets.Scripts.Levels
         public Transform playerTransform;
 
         [Header("CHUNKING PROPERTIES")]
-        public int viewDistanceInChunks = 3;
+        [Range(3, 10)] public int viewDistanceInChunks = 3;
         public int chunkSize = 150;
         [Min(1f)] public float rayDistance = 1;
         private Vector2 playerChunkCoord;
         private RaycastHit chunkHit = new RaycastHit();
         private Transform chunkParent;
 
-        // Scripts & Components
-        private RenderHelper renderHelper;
+        public Dictionary<Vector2, Chunk> loadedChunks = new Dictionary<Vector2, Chunk>();
+
+        public class Chunk
+        {
+            public GameObject meshObject;
+
+            public Chunk(Vector2 position, int size, Material material)
+            {
+                meshObject = CreateChunk(position, size, material);
+            }
+
+            public static GameObject CreateChunk(Vector2 position, int size, Material material)
+            {
+                // Specifically set for optimization, two triangles is enough
+                Vector3[] vertices = new Vector3[4];
+                int[] triangles = new int[6];
+                float halfSize = size * 0.5f; // Used to center GameObject with position cursor.
+
+                GameObject meshObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider));
+                Vector3 meshPosition = meshObject.transform.position;
+                meshObject.transform.position = new Vector3(position.x, 0, position.y);
+
+                vertices[0] = new Vector3(meshPosition.x - halfSize, 0, meshPosition.z + size - halfSize);
+                vertices[1] = new Vector3(meshPosition.x + size - halfSize, 0, meshPosition.z + size - halfSize);
+                vertices[2] = new Vector3(meshPosition.x - halfSize, 0, meshPosition.z - halfSize);
+                vertices[3] = new Vector3(meshPosition.x + size - halfSize, 0, meshPosition.z - halfSize);
+
+                triangles[0] = 0;
+                triangles[1] = 1;
+                triangles[2] = 2;
+                triangles[3] = 2;
+                triangles[4] = 1;
+                triangles[5] = 3;
+
+                Mesh mesh = new Mesh();
+                mesh.vertices = vertices;
+                mesh.triangles = triangles;
+                mesh.RecalculateNormals();
+
+                meshObject.GetComponent<MeshFilter>().mesh = mesh;
+                meshObject.GetComponent<MeshRenderer>().material = material;
+                meshObject.GetComponent<MeshCollider>().sharedMesh = mesh;
+
+                return meshObject;
+            }
+
+            public void UpdateChunkVisibility() // TODO: check if player is too far from chunk edge ig
+            {
+
+            }
+        }
 
         private void Awake()
         {
-            renderHelper = new RenderHelper();
-            renderHelper.loadedChunks.Clear(); // TODO: Hold data in save file instead of clearing.
+            loadedChunks.Clear(); // TODO: Hold data in save file instead of clearing.
             chunkParent = GameObject.Find("Chunks").transform;
 
             playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
@@ -49,7 +98,7 @@ namespace Assets.Scripts.Levels
                     Vector2 coord = new Vector2(playerChunkCoord.x + (chunkSize * x), playerChunkCoord.y + (chunkSize * y));
                     Chunk newChunk = new Chunk(coord, chunkSize, GameManager.Instance.white);
 
-                    renderHelper.loadedChunks.Add(coord, newChunk);
+                    loadedChunks.Add(coord, newChunk);
                     newChunk.meshObject.transform.parent = chunkParent;
                 }
             }
@@ -68,17 +117,17 @@ namespace Assets.Scripts.Levels
                     {
                         Vector2 coord = new Vector2(playerChunkCoord.x + (chunkSize * x), playerChunkCoord.y + (chunkSize * y));
                         
-                        if (!renderHelper.loadedChunks.ContainsKey(coord))
+                        if (!loadedChunks.ContainsKey(coord))
                         {
                             Chunk newChunk = new Chunk(coord, chunkSize, GameManager.Instance.white);
 
-                            renderHelper.loadedChunks.Add(coord, newChunk);
+                            loadedChunks.Add(coord, newChunk);
                             newChunk.meshObject.transform.parent = chunkParent;
                         }
 
                         else
                         {
-                            renderHelper.loadedChunks[coord].UpdateChunkVisibility(); // Has to be implemented
+                            loadedChunks[coord].UpdateChunkVisibility(); // Has to be implemented
                         }
                     }
                 }
--- Assets/Scripts/RenderHelper.cs
+++ Assets/Scripts/RenderHelper.cs
@@ -7,56 +7,6 @@ namespace Assets.Scripts
 {
     public class RenderHelper
     {
-        public Dictionary<Vector2, Chunk> loadedChunks = new Dictionary<Vector2, Chunk>();
-
-        public class Chunk
-        {
-            public GameObject meshObject;
-
-            public Chunk(Vector2 position, int size, Material material)
-            {
-                meshObject = CreateChunk(position, size, material);
-            }
-
-            public static GameObject CreateChunk(Vector2 position, int size, Material material)
-            {
-                // Specifically set for optimization, two triangles is enough
-                Vector3[] vertices = new Vector3[4];
-                int[] triangles = new int[6];
-                float halfSize = size * 0.5f; // Used to center GameObject with position cursor.
-
-                GameObject meshObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider));
-                Vector3 meshPosition = meshObject.transform.position;
-                meshObject.transform.position = new Vector3(position.x, 0, position.y);
-
-                vertices[0] = new Vector3(meshPosition.x - halfSize, 0, meshPosition.z + size - halfSize);
-                vertices[1] = new Vector3(meshPosition.x + size - halfSize, 0, meshPosition.z + size - halfSize);
-                vertices[2] = new Vector3(meshPosition.x - halfSize, 0, meshPosition.z - halfSize);
-                vertices[3] = new Vector3(meshPosition.x + size - halfSize, 0, meshPosition.z - halfSize);
-                
-                triangles[0] = 0;
-                triangles[1] = 1;
-                triangles[2] = 2;
-                triangles[3] = 2;
-                triangles[4] = 1;
-                triangles[5] = 3;
-
-                Mesh mesh = new Mesh();
-                mesh.vertices = vertices;
-                mesh.triangles = triangles;
-                mesh.RecalculateNormals();
-
-                meshObject.GetComponent<MeshFilter>().mesh = mesh;
-                meshObject.GetComponent<MeshRenderer>().material = material;
-                meshObject.GetComponent<MeshCollider>().sharedMesh = mesh;
-
-                return meshObject;
-            }
-
-            public void UpdateChunkVisibility() // TODO: check if player is too far from chunk edge ig
-            {
-
-            }
-        }
+        
     }
 }
